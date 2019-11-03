e7fe60c4a0e7d4aab95b88598f04ba204e3c17fd
Rok Povsic <rok.povsic@gmail.com>
2019-11-03 21:55:19 +0100

    Remove taskbar from full screen size

--- WinTiler/Overlay/FullScreen.cs
+++ WinTiler/Overlay/FullScreen.cs
@@ -1,4 +1,4 @@
-using System.Windows.Forms;
+using System.Windows;
 
 namespace WinTiler.Overlay
 {
@@ -6,8 +6,8 @@ namespace WinTiler.Overlay
     {
         private static int NUM_OF_BOXES = 4;
 
-        public static int ScreenWidth => Screen.PrimaryScreen.Bounds.Width;
-        public static int ScreenHeight => Screen.PrimaryScreen.Bounds.Height;
+        public static int ScreenWidth => (int)SystemParameters.WorkArea.Width;
+        public static int ScreenHeight => (int)SystemParameters.WorkArea.Height;
 
         public static int BoxWidth => ScreenWidth / NUM_OF_BOXES;
         public static int BoxHeight => ScreenHeight / NUM_OF_BOXES;
