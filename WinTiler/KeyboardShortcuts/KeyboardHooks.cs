7a21d24445784f378fde050bc9e413000195e36e
Rok Povsic <rok.povsic@gmail.com>
2020-04-11 10:34:44 +0200

    Use guard clause if

--- WinTiler/KeyboardShortcuts/KeyboardHooks.cs
+++ WinTiler/KeyboardShortcuts/KeyboardHooks.cs
@@ -23,71 +23,68 @@ namespace WinTiler.KeyboardShortcuts
 
         private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
         {
-            if (e.KeyboardState != GlobalKeyboardHook.KeyboardState.SysKeyDown)
+            if (!(e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown && GlobalKeyboardHook.WinAltPressed))
                 return;
 
-            if (GlobalKeyboardHook.WinAltPressed)
-            {
-                int width = FullScreen.ScreenWidth;
-                int height = FullScreen.ScreenHeight;
+            int width = FullScreen.ScreenWidth;
+            int height = FullScreen.ScreenHeight;
 
-                var win = new WindowManipulation();
-                switch (e.KeyboardData.Key)
+            var win = new WindowManipulation();
+            switch (e.KeyboardData.Key)
+            {
+                case Keys.I:
                 {
-                    case Keys.I:
-                    {
-                        win.SetForegroundPosSize(width / 2, 0, width / 2, height / 2);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.U:
-                    {
-                        win.SetForegroundPosSize(0, 0, width / 2, height / 2);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.N:
-                    {
-                        win.SetForegroundPosSize(0, height / 2, width / 2, height / 2);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.M:
-                    {
-                        win.SetForegroundPosSize(width / 2, height / 2, width / 2, height / 2);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.J:
-                    {
-                        win.SetForegroundPosSize(width / 4, height / 4, width / 2, height / 2);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.K:
-                    {
-                        win.SetForegroundPosSize(width / 4, 0, width / 2, 3 * height / 4);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.H:
-                    {
-                        win.SetForegroundPosSize(0, 0, width / 2, 3 * height / 4);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.L:
-                    {
-                        win.SetForegroundPosSize(width / 2, 0, width / 2, 3 * height / 4);
-                        e.Handled = true;
-                        break;
-                    }
-                    case Keys.Enter:
-                    {
-                        _mainWindow.Show();
-                        e.Handled = true;
-                        break;
-                    }
+                    win.SetForegroundPosSize(width / 2, 0, width / 2, height / 2);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.U:
+                {
+                    win.SetForegroundPosSize(0, 0, width / 2, height / 2);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.N:
+                {
+                    win.SetForegroundPosSize(0, height / 2, width / 2, height / 2);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.M:
+                {
+                    win.SetForegroundPosSize(width / 2, height / 2, width / 2, height / 2);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.J:
+                {
+                    win.SetForegroundPosSize(width / 4, height / 4, width / 2, height / 2);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.K:
+                {
+                    win.SetForegroundPosSize(width / 4, 0, width / 2, 3 * height / 4);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.H:
+                {
+                    win.SetForegroundPosSize(0, 0, width / 2, 3 * height / 4);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.L:
+                {
+                    win.SetForegroundPosSize(width / 2, 0, width / 2, 3 * height / 4);
+                    e.Handled = true;
+                    break;
+                }
+                case Keys.Enter:
+                {
+                    _mainWindow.Show();
+                    e.Handled = true;
+                    break;
                 }
             }
         }
